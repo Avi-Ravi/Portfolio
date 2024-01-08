9816ba01b979ea40c40fb3c47951dbeb2b41f2d8
Ravi Kumar <106535452+Avi-Ravi@users.noreply.github.com>
2024-01-08 18:38:41 +0530

    Update GSC

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/app.scss";
-
+<meta name="google-site-verification" content="CqcVDSAW4S4zz8PO0hio9ZMCiPnVKenDjOFBXieceSA" />
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
